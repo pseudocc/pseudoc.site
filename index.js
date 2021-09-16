a895b5425cda49a7c9d4290877b05bd8936cff24
Yu Ze <pseudoc@163.com>
2021-09-16 21:51:57 +0800

    proxy

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ if (require.main == module) {
   const env = try_get_env();
   const Koa = require('koa');
   const Router = require('@koa/router');
+
   const favicon = require('koa-favicon');
-  
+  const proxy = require('koa-proxy');
+
   const app = new Koa();
   const router = new Router();
+
+  assert.ok(env.SWORDJS_PORT !== undefined);
+  router.all(/^\/swordjs\/*.*/, proxy({
+    host: `http://localhost:${env.SWORDJS_PORT}`,
+    map: p => p.replace('/swordjs', '')
+  }));
 
   router.get('/', async ctx => {
     ctx.body = 'Hello, world!';
@@ -20,11 +28,11 @@ if (require.main == module) {
     .use(favicon(path.join(__dirname, 'assets', 'pseudoc.ico')))
     .use(router.routes())
     .use(router.allowedMethods());
-  
+
   assert.ok(env.HTTP_PORT !== undefined);
   const http_server = http.createServer(app.callback());
   http_server.listen(env.HTTP_PORT, () => {
-    console.log('HTTP server is hosting on port %d', env.HTTP_PORT);
+    console.log('HTTP server is hosting on port %s', env.HTTP_PORT);
   });
 
   if (env.HTTPS_PORT !== undefined) {
@@ -35,13 +43,16 @@ if (require.main == module) {
     };
     const https_server = https.createServer(options, app.callback());
     https_server.listen(env.HTTPS_PORT, () => {
-      console.log('HTTPS server is hosting on port %d', env.HTTPS_PORT);
+      console.log('HTTPS server is hosting on port %s', env.HTTPS_PORT);
     });
   }
 }
 
 function try_get_env() {
-  const env_output = require('dotenv').config();
+  const load_opt = process.env.DEBUG
+    ? { path: path.join(__dirname, '.debug.env') }
+    : undefined;
+  const env_output = require('dotenv').config(load_opt);
   if (env_output.error) {
     console.error(env_output.error);
     process.exit(1);
